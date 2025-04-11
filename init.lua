619ca835d9fc5de8df4eff1cbab57f7d29a57ad0
mustaphouni04 <elaichounimustapha@gmail.com>
2025-04-11 19:14:40 +0200

    add molten, change theme

--- init.lua
+++ init.lua
@@ -7,3 +7,5 @@ require("config.settings")
 require("config.keymaps")
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
+vim.g.python3_host_prog=vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
+vim.cmd("colorscheme tokyonight-day")
