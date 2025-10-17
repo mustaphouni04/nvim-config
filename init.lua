91feeb049cf1b3d5235b22d95d56952cfb81bd72
melaichouni <melaichouni@miteksystems.com>
2025-10-17 11:38:52 +0000

    add miasma color theme

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 local on_attach = require("config.on_attach").on_attach
-require("config.lazy") 
+require("config.lazy")
 require("config.theme")
 -- require("config.treesitter")
 require("config.lsp").setup_all(on_attach)
@@ -11,4 +11,5 @@ require("config.keymaps")
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
 vim.g.python3_host_prog=vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
-vim.cmd("colorscheme tokyonight-day")
+vim.cmd("colorscheme miasma")
+
